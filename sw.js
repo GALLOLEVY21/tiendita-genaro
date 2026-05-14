3dbadd1c9fc36ff8d73b6c0011cc4cf730407e55
GALLOLEVY21 <clevy21@gmail.com>
2026-05-14 12:29:53 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "tiendita-v1";
+const ASSETS = ["/tiendita-genaro/", "/tiendita-genaro/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
